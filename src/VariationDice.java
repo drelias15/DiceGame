e44f2cfeb8a0c70759c1a89bc5014584fc13511f
drelias15 <drelias15@gmail.com>
2018-07-18 11:38:40 -0400

    edited

--- src/VariationDice.java
+++ src/VariationDice.java
@@ -3,41 +3,67 @@ import java.util.Scanner;
 
 public class VariationDice {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int score = 0;
         Scanner keyboard = new Scanner(System.in);
         Random rand = new Random();
 
-        System.out.println("Press any key to start. Press Q to quit rolling. Press Y to continue rolling");
+        System.out.println("Rolling Dice....\nPress any key to start. Press Q to quit rolling. Press Y to continue rolling");
         String input = keyboard.next();
         int ran1;
         int ran2;
         int sum = 0;
 
 
-
-     do {
-
-
-            if (!input.equalsIgnoreCase("q")){
-
+        do {
             ran1 = 1 + rand.nextInt(6);
             ran2 = 1 + rand.nextInt(6);
-            sum = (ran1+ran2)/2;
-            score = sum*25;
 
-            System.out.println(" You roll: " + ran1 + " " + ran2+". Your score is " + score);
-            System.out.println("press Y to try ");
-            input = keyboard.next();
+            if (ran1 != 1 && ran2 != 1) {
+                score = (ran1 + ran2) + score;
 
-            }else{
-                System.out.println("");
-                break;
+                System.out.println(" You roll: " + ran1 + " " + ran2 + ". Your score is " + score);
+                System.out.println("press Y to try  or Q to end.");
+
+                input = keyboard.next();
+
+
+            } else if (ran1 == 1 && ran2 != 1) {
+                score = ran2 + score;
+
+                System.out.println(" You roll: " + ran1 + " " + ran2 + ". Your score is " + score);
+                System.out.println("press Y to try ");
+
+                input = keyboard.next();
+
+            } else if (ran1 != 1 && ran2 == 1) {
+                score = ran1 + score;
+
+                System.out.println(" You roll: " + ran1 + " " + ran2 + ". Your score is " + score);
+                System.out.println("press Y to try ");
+
+                input = keyboard.next();
+
+            } else {
+                score = 25 + score;
+
+                System.out.println(" You roll: " + ran1 + " " + ran2 + ". Your score is " + score);
+                System.out.println("press Y to try ");
+
+                input = keyboard.next();
+
             }
 
-        }  while (score < 100);
 
-        System.out.println("Thank you. Your score is " + score + ". Game ends here");
+        } while (!input.equalsIgnoreCase("q") && score < 100);
+
+
+        if (input.equalsIgnoreCase("q")){
+            System.out.println("Thank you for playing the game. Your score was " + score +"." );
+        }else{
+            System.out.println("Congratulations! Your score is " + score + ".");
+        }
+
     }
 
 }
